9f740c87bfbe3d2e3d2d0d7731fbf1f87b77067c
aethletic <gorshkov34@gmail.com>
2020-12-12 17:09:00 +0400

    init

---
+++ src/Client.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TelegramApiServer;
+
+class Client
+{
+    private $config = [
+        'host' => '127.0.0.1',
+        'port' => '9503',
+        'session' => 'session',
+    ];
+
+    private $curl;
+
+    public function __construct(array $config = null)
+    {
+        $this->config = array_merge($this->config, $config);
+        $this->curl = curl_init();
+    }
+
+    public function post(string $method, array $params = []) : array
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->buildApiUrl($method));
+        curl_setopt($this->curl, CURLOPT_POST, true);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($params));
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($this->curl);
+
+        return json_decode($response, true);
+    }
+
+    public function get(string $method, array $params = []) : array
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->buildApiUrl($method) . '?' . http_build_query($params));
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($this->curl);
+
+        return json_decode($response, true);
+    }
+
+    private function buildApiUrl($method): string
+    {
+        return "{$this->config['host']}:{$this->config['port']}/api/{$method}";
+    }
+}
